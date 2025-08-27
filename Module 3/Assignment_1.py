cf959e74335537aa2394b45647e061f5f350e0a4
hjrik <hjrikardsen@gmail.com>
2025-08-27 10:38:02 +0300

    Module 3

--- Module 3/Assignment_1.py
+++ Module 3/Assignment_1.py
@@ -3,6 +3,6 @@ zander = float(input("Enter the length of the zander in centimeters: "))
 if zander < 42:
     print("The zander does not meet the size limit.")
     print("Please release the fish back into the lake.")
-    print(f"The fish was {round(42 - zander, 2)} centimeters below the size limit.")
+    print(f"The fish was {round(42 - zander, 1)} centimeters below the size limit.")
 else:
     print("The zander meets the size limit.")
