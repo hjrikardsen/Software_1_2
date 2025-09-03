310d9ebd3b57b284923a9a89cdfb5df2414461b5
hjrik <hjrikardsen@gmail.com>
2025-09-03 11:31:40 +0300

    Module 5

---
+++ Module 5/Assignment_4.py
@@ -0,0 +1,9 @@
+cities_list = []
+
+for i in range(0, 5):
+    city = input("Enter the name of a city: ")
+    cities_list.append(city)
+
+print("\n\nThe cities you entered:")
+for i in cities_list:
+    print(i)
